f8d42ea0c6538254bd5df2142178b182710ee247
Mokiy <92001109+MokiyCodes@users.noreply.github.com>
2021-11-10 19:25:31 +0100

    Create index.js

---
+++ z/index.js
@@ -0,0 +1,22 @@
+(()=>{
+  /* Search for arg */
+  let url;
+  const s = window.location.search != '' && window.location.search.length > 1 ? (window.location.search+window.location.hash.replace('#','&')) : window.location.hash.replace('#','?')
+  if (window.location.search != '' && window.location.hash != '') {
+    const params = new URLSearchparams(s);
+    const searchFor = ["u","url","e","enc","encoded","d","dest","destination","goto","gt"];
+    searchFor.forEach((el)=>{
+      if (url) return;
+      const g = params.get(el);
+      if (g && g!='') {
+        url=g;
+      }
+    })
+  }
+  /* Redirect/Show Tool */
+  if (url && url !== ''){
+    setTimeout(()=>document.location.href=window.zwspSteg.decode(decodeURI(url)),100)
+  }else{
+    document.addEventListener('DOMContentLoaded',()=>document.querySelector('.CreationTool').hidden=null)
+  }
+})()
